ea8de9b556f8bc2a06bd1e6d38dc3b536ea8dd90
SebastianIRodriguez <seba.rodriguez.994@gmail.com>
2023-09-27 16:10:08 -0300

    Completo implementacion de feedforward y predictor de smith

--- TP1/MATLAB/Seccion 4 - Predictor de Smith/simular_sistemas.m
+++ TP1/MATLAB/Seccion 4 - Predictor de Smith/simular_sistemas.m
@@ -1,8 +1,8 @@
-function [ CE_SMITH, CE_DEFAULT ] = simular_sistemas(...
-Kc_Smith,Tau_Smith,Kc_Normal,Tau_Normal,Fh0...
-)
-sim_results = sim('Predictor_de_smith.slx', 'SrcWorkspace', 'current');
-CE_DEFAULT = sim_results.get('CE_DEFAULT');
-CE_SMITH = sim_results.get('CE_SMITH');
+function [ out_SMITH, out_DEFAULT ] = simular_sistemas()
+load('backup_workspace');
+sim_results = sim('Predictor_de_smith_con_planta.slx', 'SrcWorkspace', 'current');
+out_SMITH = sim_results.get('CE_SMITH');
+%sim_results = sim('solo_feedback.slx', 'SrcWorkspace', 'current');
+out_DEFAULT = [];%sim_results.get('CE_DEFAULT');
 end
 
